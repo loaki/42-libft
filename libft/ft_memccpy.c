f3e4fafd1a3de921f63d2bbc28b8d0e4e63ac37f
Macbook pro <macbookpro@MacBook-Pro-de-Macbook.local>
2019-10-27 12:00:01 +0100

    memccpy

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -1,7 +1,7 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	int				i;
 	unsigned char * str1;
@@ -12,10 +12,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	str2 = (unsigned char *)dest;
 	while (n > 0)
 	{
+		if (str1[i] == (unsigned char)c)
+			return ((void *)&str1[i + 1]);
 		str2[i] = str1[i];
 		i++;
 		n--;
 	}
-	dest = (void *)str2;
-	return (dest);
+	return (NULL);
 }
